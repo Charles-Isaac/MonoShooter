38476905ecc509059f2a60a261c5ec5e0fac3207
clonecharle1 <charl_i_c@hotmail.com>
2017-03-20 21:12:34 -0400

    Demo for monogame

---
+++ MonoShooterTest/Bullet.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace MonoShooterTest
+{
+    class Bullet
+    {
+        private int m_TransitionStartTime;
+        private int m_CurrentTime;
+        private Vector2 m_StartPosition;
+        private int m_RightMargin;
+
+        public Bullet(int TransitionStartTime, Vector2 Position, int RightMargin)
+        {
+            m_TransitionStartTime = TransitionStartTime;
+            m_CurrentTime = TransitionStartTime;
+            m_StartPosition = Position;
+            m_RightMargin = RightMargin;
+        }
+
+        public bool Update(int NewCurrentTime)
+        {
+            m_CurrentTime = NewCurrentTime;
+            return (Position.X >= m_RightMargin);
+
+        }
+        public int Transition
+        {
+            get
+            {
+                if (((m_CurrentTime - m_TransitionStartTime) / 100) % 4 > 4 || ((m_CurrentTime - m_TransitionStartTime) / 100) % 4 < 0)
+                {
+                    int i = 0;
+                }
+                return (((m_CurrentTime - m_TransitionStartTime) / 100) % 4);
+            }
+        }
+
+        public Vector2 Position
+        {
+            get { return m_StartPosition + new Vector2(0.3f *(m_CurrentTime - m_TransitionStartTime), 0); }
+        }
+
+        public int CurrentTime
+        {
+            get { return m_CurrentTime; }
+            set { m_CurrentTime = value; }
+        }
+    }
+}
---
+++ MonoShooterTest/Shooter.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoShooterTest
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Shooter : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        private Texture2D[] PlayerTexture;
+        private Texture2D[] BulletTexture;
+        private Texture2D StarTexture;
+        private Vector2 PlayerPosition;
+        private List<Bullet> BulletList;
+        private int PlayerLastShot;
+
+        public Shooter()
+        {
+            graphics = new GraphicsDeviceManager(this);
+
+            graphics.IsFullScreen = true;
+
+            graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            graphics.ApplyChanges();
+
+          //  graphics.ToggleFullScreen();
+            Content.RootDirectory = "Content";
+            PlayerPosition = new Vector2(50,50);
+            BulletList = new List<Bullet>();
+            PlayerLastShot = 0;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            
+
+            PlayerTexture = new Texture2D[4];
+            BulletTexture = new Texture2D[4];
+
+            // TODO: use this.Content to load your game content here
+
+            PlayerTexture[0] = Content.Load<Texture2D>(@"Texture/Player/Player1");
+            PlayerTexture[1] = Content.Load<Texture2D>(@"Texture/Player/Player2");
+            PlayerTexture[2] = Content.Load<Texture2D>(@"Texture/Player/Player3");
+            PlayerTexture[3] = Content.Load<Texture2D>(@"Texture/Player/Player2");
+
+
+            BulletTexture[0] = Content.Load<Texture2D>("Texture/Bullet/Bullet1");
+            BulletTexture[1] = Content.Load<Texture2D>("Texture/Bullet/Bullet2");
+            BulletTexture[2] = Content.Load<Texture2D>("Texture/Bullet/Bullet3");
+            BulletTexture[3] = Content.Load<Texture2D>("Texture/Bullet/Bullet4");
+
+            StarTexture = Content.Load<Texture2D>("Texture/Background/StarBackground");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W) || Keyboard.GetState().IsKeyDown(Keys.A) ||
+                Keyboard.GetState().IsKeyDown(Keys.S) || Keyboard.GetState().IsKeyDown(Keys.D) || 
+                Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                if (Keyboard.GetState().IsKeyDown(Keys.W))
+                {
+                    PlayerPosition += new Vector2(0, -0.1f * gameTime.ElapsedGameTime.Milliseconds);
+                }
+
+                if (Keyboard.GetState().IsKeyDown(Keys.A))
+                {
+                    PlayerPosition += new Vector2(-0.1f * gameTime.ElapsedGameTime.Milliseconds, 0);
+                }
+
+                if (Keyboard.GetState().IsKeyDown(Keys.S))
+                {
+                    PlayerPosition += new Vector2(0, 0.1f * gameTime.ElapsedGameTime.Milliseconds);
+                }
+
+                if (Keyboard.GetState().IsKeyDown(Keys.D))
+                {
+                    PlayerPosition += new Vector2(0.1f * gameTime.ElapsedGameTime.Milliseconds, 0);
+                }
+
+                if (Keyboard.GetState().IsKeyDown(Keys.Space) && gameTime.TotalGameTime.TotalMilliseconds - PlayerLastShot > 500)
+                {
+                    PlayerLastShot = (int) gameTime.TotalGameTime.TotalMilliseconds;
+                    BulletList.Add(new Bullet((int) gameTime.TotalGameTime.TotalMilliseconds,
+                        PlayerPosition +
+                        new Vector2(PlayerTexture[(gameTime.TotalGameTime.Milliseconds/250)%4].Width - 6,
+                            PlayerTexture[(gameTime.TotalGameTime.Milliseconds/250)%4].Height/2.0f),
+                        GraphicsDevice.PresentationParameters.BackBufferWidth + 10));
+                }
+            }
+
+            for (int i = BulletList.Count - 1; i >= 0; i--)
+            {
+                if (BulletList[i].Update((int)gameTime.TotalGameTime.TotalMilliseconds))
+                {
+                    BulletList.RemoveAt(i);
+                }
+            }
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            
+            spriteBatch.Begin();
+
+            for (int i = 0; i < 2; i++)
+            {
+                spriteBatch.Draw(StarTexture,
+                    new Rectangle(
+                        (int)
+                        -(gameTime.TotalGameTime.TotalMilliseconds/2%
+                          GraphicsDevice.PresentationParameters.BackBufferWidth) +
+                        GraphicsDevice.PresentationParameters.BackBufferWidth*i - 1, 0,
+                        GraphicsDevice.PresentationParameters.BackBufferWidth + 2,
+                        GraphicsDevice.PresentationParameters.BackBufferHeight), Color.White);
+            }
+
+            spriteBatch.Draw(PlayerTexture[(gameTime.TotalGameTime.Milliseconds/150)%4],
+                new Rectangle((int) PlayerPosition.X, (int) PlayerPosition.Y,
+                    PlayerTexture[(gameTime.TotalGameTime.Milliseconds/150)%4].Width,
+                    PlayerTexture[(gameTime.TotalGameTime.Milliseconds/150)%4].Height), Color.White);
+            for (int i = BulletList.Count - 1; i >= 0; i--)
+            {
+                spriteBatch.Draw(BulletTexture[BulletList[i].Transition],
+                    new Rectangle((int) BulletList[i].Position.X, (int) BulletList[i].Position.Y,
+                        BulletTexture[BulletList[i].Transition].Width, BulletTexture[BulletList[i].Transition].Height),
+                    Color.White);
+            }
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+
+    }
+}
